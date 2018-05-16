9e381724870adaa1de9f750267cfd4fd8e0f04cd
classyp <jeanphilip@mecom>
2018-05-15 19:19:10 -0700

    trying to make jumbotron work but failing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Jumbotron } from './Components/Jumbotron/Jumbotron.js'
+
+
 class App extends Component {
   render() {
     return (
+      <Jumbotron />
 
     );
   }
