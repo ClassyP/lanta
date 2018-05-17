ea4de2d0e982dec0536d33f3dc991374ffb08f58
classyp <jeanphilip@mecom>
2018-05-17 16:53:14 -0700

    adding navbar to jumtron

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Jumbotron, Container } from 'reactstrap';
+// import { Navbar } from './Navbar/Navbar.js'
+
 
 export default class Header extends React.Component {    
 
@@ -8,6 +10,7 @@ export default class Header extends React.Component {
         <div>
           <Jumbotron fluid>
             <Container fluid>
+              {/* <Navbar /> */}
               <h1 className="display-3">LANTA CONSULTANTS</h1>
               <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
             </Container>
